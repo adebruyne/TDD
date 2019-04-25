d829f16fb1ba40ebb9ca755ce8620ccf53615f47
Aylin DeBruyne <adebruyne88@gmail.com>
2019-04-25 10:16:25 -0400

    adds create action to post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
 end
